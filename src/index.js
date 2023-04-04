c3836ab679a1e3955b938cce04bb88294a912bf1
Taylor Todd <ttodd1031@gmail.com.com>
2023-04-04 14:57:57 -0500

    added default grocery project

--- src/index.js
+++ src/index.js
@@ -6,11 +6,9 @@ let myProjects = [];
 
 function init(defaultProjects) {
     let defaultProject = new Project("Daily", "Your daily To-do list.");
-    console.log(defaultProject);
+    let defaultGroceries = new Project("Groceries", "Shopping List for the week", null);
     let toDo1 = new toDo("Walk the dog", "Take fido for a walk", "March 5", "Urgent");
-    console.log(toDo1);
-    defaultProject.list.push(toDo1);
-    myProjects.push(defaultProject);
+    myProjects.push(defaultProject, defaultGroceries);
     console.log(defaultProject);
     updateSidebar();
 }
