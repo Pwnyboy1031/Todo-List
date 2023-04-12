c19c5b8578d4093b83bb47befaf3ce985746974f
Taylor Todd <ttodd1031@gmail.com.com>
2023-04-11 21:47:36 -0500

    adding tasks now updates them in the main window

---
+++ src/mainLoad.js
@@ -0,0 +1,46 @@
+import { projectDictionary } from "./project";
+
+function mainLoad() {
+    // reset main content
+    const main = document.getElementById("main");
+    main.innerHTML = "";
+    
+
+    const projectName = document.querySelector(".selected").textContent;
+    const workingProject = projectDictionary[projectName];
+
+    for (let todo in workingProject.list) {
+        const workingTodo = document.createElement("div");
+        workingTodo.setAttribute("class", "mainTodo");
+        workingTodo.setAttribute("id", `${workingProject.list[todo].title}`);
+
+        // title 
+        const title = document.createElement("div");
+        title.setAttribute("class", "todoTitle");
+        title.textContent = workingProject.list[todo].title;
+        workingTodo.appendChild(title);
+
+        // description
+        const description = document.createElement("div");
+        description.setAttribute("class", "todoDescription");
+        description.textContent = workingProject.list[todo].description;
+        workingTodo.appendChild(description);
+
+        // due date
+        const dueDate = document.createElement("div");
+        dueDate.setAttribute("class", "todoDueDate");
+        dueDate.textContent = workingProject.list[todo].dueDate;
+        workingTodo.appendChild(dueDate);
+
+        // checkbox
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.setAttribute("class", "todoCheckbox");
+        workingTodo.appendChild(checkbox);
+
+
+        main.appendChild(workingTodo);
+    }
+};
+
+export {mainLoad};
